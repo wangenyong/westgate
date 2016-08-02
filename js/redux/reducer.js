8bede1153a00f796934b2f9d97a8332b0ba2cfd2
wangenyong <wangenyong@gmail.com>
2016-08-02 16:56:55 +0800

    Create reducer.js

---
+++ js/redux/reducer.js
@@ -0,0 +1,30 @@
+import {Map} from 'immutable';
+import {combineReducers} from 'redux-loop';
+import SessionStateReducer, {RESET_STATE} from '../modules/session/SessionState';
+
+const reducers = {
+
+  session: SessionStateReducer
+
+};
+
+// initial state, accessor and mutator for supporting root-level
+// immutable data with redux-loop reducer combinator
+const immutableStateContainer = Map();
+const getImmutable = (child, key) => child ? child.get(key) : void 0;
+const setImmutable = (child, key, value) => child.set(key, value);
+
+const namespacedReducer = combineReducers(
+  reducers,
+  immutableStateContainer,
+  getImmutable,
+  setImmutable
+);
+
+export default function mainReducer(state, action) {
+  if (action.type === RESET_STATE) {
+    return namespacedReducer(action.payload, action);
+  }
+
+  return namespacedReducer(state || void 0, action);
+}
