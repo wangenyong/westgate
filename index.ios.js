857a9f5e93d726712f5936f1b148ffa83183a9b9
wangenyong <wangenyong@gmail.com>
2016-08-03 10:00:22 +0800

    修改index.ios.js显示setup页面

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,9 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import {Provider} from 'react-redux';
+import store from './js/redux/store';
+import Setup from './js/setup';
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,42 +14,15 @@ import {
   View
 } from 'react-native';
 
-class westgate extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const westgate = React.createClass({
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  render: function() {
+    return (
+      <Provider store={store}>
+        <Setup />
+      </Provider>
+    )
+  }
+})
 
 AppRegistry.registerComponent('westgate', () => westgate);
