91d03fc12af668d417e493f4f0583d01d1191395
wangenyong <wangenyong@gmail.com>
2016-08-10 15:26:02 +0800

    Install react-native-vector-icons

---
+++ js/utils/colorUtil.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkPrimaryColor: '#1976d2',
+  primaryColor: '#2196f3',
+  lightPrimaryColor: '#bbdefb',
+  accentColor: '#cddc39',
+  textIcon: '#ffffff',
+  textPrimary: '#212121',
+  textSecondary: '#757575',
+  dividerColor: '#b5b5b5',
+  darkText: '#032250',
+  lightText: '#7F91A7'
+}
